6c85f95fee9318238b57229e6879b76be1069b37
matias9205 <matiasmazparrote@hotmail.com>
2025-06-23 16:55:35 -0300

    add config file for read.env file with DB credentials and db.py file for connect to DB

---
+++ app/config/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    debug: bool = False
+
+    @property
+    def database_url(self) -> str:
+        return f"mysql+pymysql://{self.mysql_user}:{self.mysql_password}@{self.mysql_host}:3306/{self.mysql_db}"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+settings = Settings()
