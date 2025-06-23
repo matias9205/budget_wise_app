59f694f5095468bf8b7e9288a566c92b0160d72d
matias9205 <matiasmazparrote@hotmail.com>
2025-06-23 16:57:00 -0300

    add init.sql for load categories into the table

---
+++ SQL/init.sql
@@ -0,0 +1,10 @@
+-- Active: 1729102093806@@127.0.0.1@3306@BudgetWiseAPI
+USE BudgetWiseAPI;
+
+INSERT INTO categories (name) VALUES
+('Alquiler'),
+('Expensas'),
+('Facturas de servicios'),
+('Viajes'),
+('Compras'),
+('Sueldos');
