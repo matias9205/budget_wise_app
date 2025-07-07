c55ef9479e9fe754187ab184eb0ce4a8af5a235a
matias9205 <matiasmazparrote@hotmail.com>
2025-07-07 15:32:52 -0300

    update post new transaction route by adding token dependence

--- app/transactions/routers.py
+++ app/transactions/routers.py
@@ -2,14 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.categories.models import Category
+from app.config.config import Settings
 from app.config.db import get_db
 from app.transactions.services import TransactionService
+from app.utils.security import Security
 from .schemas import TransactionSchema, NewTransactionSchema 
 from .models import Transaction
 from app.users.models import User
 
-class TransactionRouter:
+class TransactionRouter(Security):
     def __init__(self):
+        super().__init__(Settings().SECRET_KEY)
         self.transaction_router = APIRouter(tags=["transactions"])
         self._add_routes()
 
@@ -23,5 +26,9 @@ class TransactionRouter:
             return TransactionService(db).get_transaction(id)
         
         @self.transaction_router.post("/new", response_model=TransactionSchema)
-        def register_new_transaction(payload: NewTransactionSchema, db: Session = Depends(get_db)):
-            return TransactionService(db).create_new_transaction(payload)
+        def register_new_transaction(payload: NewTransactionSchema, db: Session = Depends(get_db), token: str = Depends(self.get_oauth2_scheme())):
+            print(f"ACCESS TOKEN IN auth user POST: {token}")
+            user = self.get_current_user(token)
+            print(f"User from token: {user['sub']}")
+            print(f"PAYLOAD FOR A NEW TRANSACTION - IN POST ROUTER: {payload.__dict__}")
+            return TransactionService(db).create_new_transaction(user['username'], payload)
