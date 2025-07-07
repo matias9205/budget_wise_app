b0de1d191ec08ec0637ade115963c8ada4383552
matias9205 <matiasmazparrote@hotmail.com>
2025-07-07 15:35:04 -0300

    update user router by adding token dependence in create new user, update and delete

--- app/users/routers.py
+++ app/users/routers.py
@@ -2,13 +2,17 @@ from uuid import UUID
 from fastapi import APIRouter, Body, Depends, HTTPException
 from sqlalchemy.orm import Session, joinedload
 
+from app.auth.schemas import UserTokenPayload
+from app.config.config import Settings
 from app.config.db import get_db
 from app.transactions.models import Transaction
 from app.users.schemas import NewUserSchema, UpdateUser, UserSchemaWithTransactions
 from app.users.services import UserService
+from app.utils.security import Security
 
-class UserRouter:
+class UserRouter(Security):
     def __init__(self):
+        super().__init__(Settings().SECRET_KEY)
         self.user_router = APIRouter()
         self._add_routes()
 
@@ -21,14 +25,27 @@ class UserRouter:
         def fetch_one_user(id:int, db: Session = Depends(get_db)):
             return UserService(db).get_user(id)
         
+        @self.user_router.post("/auth_user", response_model=UserSchemaWithTransactions)
+        def fetch_current_user(db: Session = Depends(get_db), token: str = Depends(self.get_oauth2_scheme())):
+            print(f"ACCESS TOKEN IN auth user POST: {token}")
+            user = self.get_current_user(token)
+            print(f"User from token: {user['sub']}")
+            return UserService(db).get_user(user['sub'])
+        
         @self.user_router.post("/new", response_model=UserSchemaWithTransactions)
         def register_new_user(payload_: NewUserSchema, db: Session = Depends(get_db)) -> UserSchemaWithTransactions:
             return UserService(db).create_new_user(payload_)
         
-        @self.user_router.patch("/update/{id}", response_model=UserSchemaWithTransactions)
-        def update_user(id:int, payload: UpdateUser = Body(...), db: Session = Depends(get_db)) -> UserSchemaWithTransactions:
-            return UserService(db).update_user(id, payload)
+        @self.user_router.patch("/update", response_model=UserSchemaWithTransactions)
+        def update_user(id:int, payload: UpdateUser = Body(...), db: Session = Depends(get_db), token: str = Depends(self.get_oauth2_scheme())) -> UserSchemaWithTransactions:
+            print(f"ACCESS TOKEN IN auth user POST: {token}")
+            user = self.get_current_user(token)
+            print(f"User from token: {user['sub']}")
+            return UserService(db).update_user(user['sub'], payload)
         
-        @self.user_router.delete("/delete/{id}")
-        def delete_user(id:int, db: Session = Depends(get_db)):
-            return UserService(db).delete_user(id)
+        @self.user_router.delete("/delete")
+        def delete_user(id:int, db: Session = Depends(get_db), token: str = Depends(self.get_oauth2_scheme())):
+            print(f"ACCESS TOKEN IN auth user POST: {token}")
+            user = self.get_current_user(token)
+            print(f"User from token: {user['sub']}")
+            return UserService(db).delete_user(user['sub'])
